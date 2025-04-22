66c4b732cfbcd965e11dc22f36ff2194002abacd
Kavya Verma <kavyaverma5601@gmail.com>
2025-04-22 14:10:09 -0400

    vercel fix

--- app/[alias]/page.tsx
+++ app/[alias]/page.tsx
@@ -3,7 +3,7 @@ import clientPromise from '@/lib/mongodb';
 
 export const dynamic = 'force-dynamic';
 
-export default async function Page({ params }: { params: { alias: string } }) {
+const AliasPage = async ({ params }: { params: { alias: string } }) => {
   const client = await clientPromise;
   const db = client.db();
   const collection = db.collection('urls');
